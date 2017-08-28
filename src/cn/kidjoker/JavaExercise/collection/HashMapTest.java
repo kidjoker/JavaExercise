3ac6229f013bb75dbb0d5008c7b55c755ed97f7d
kidjoker <13621952184@163.com>
2017-08-28 09:19:36 +0800

    jason

---
+++ src/cn/kidjoker/JavaExercise/collection/HashMapTest.java
@@ -0,0 +1,29 @@
+package cn.kidjoker.JavaExercise.collection;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class HashMapTest {
+	
+	@Test public void test() {
+		Set<Object> set = new HashSet<>();
+		set.add("123");
+		set.add("456");
+		set.add("789");
+//		Map<String, Object> map = new HashMap<>(8,0.5f);
+//		map.put("hello1", "world");
+//		map.put("hello2", "world");
+//		map.put("hello3", "world");
+//		map.put("hello4", "world");
+//		map.put("hello5", "world");
+//		map.get("hello");
+	}
+	
+	@Test public void test2() {
+		System.out.println(null == null);
+	}
+}
